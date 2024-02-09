a3dac73162fae81d6430f170f844d1521277ccbf
Guy-Georges Adou Bogolo <69226314+GuyAdou@users.noreply.github.com>
2024-02-09 12:21:00 -0600

    Update MessageManager.py

--- MessageManager.py
+++ MessageManager.py
@@ -35,12 +35,7 @@ class MessageManager:
                 # Send the initial conversation message
                 await user.send("Hi there!")
                 await user.send("Did you find a roommate or an apartment? Answer 'Yes' or 'No'. Thanks!")
-
-
-                # Get the current time
                 current_time_str = datetime.now().strftime('%m/%d/%Y %H:%M:%S')
-
-                # Calculate and store the next send time for this user
                 self.scheduler.calculate_and_store_next_send_time(username, current_time_str)
             else:
                 print(f"User not found: {username}")
@@ -59,26 +54,21 @@ class MessageManager:
             if deadline_str:
                 deadline = datetime.strptime(deadline_str, '%Y-%m-%dT%H:%M:%S.%f')
                 if datetime.utcnow() > deadline:
-                    # If the user didn't respond in time, notify them and calculate the next contact time
                     await message.author.send(
                         "You did not answer in time. I'll touch base with you again in one week and get an update on your search. Good luck!."
                     )
                 else:
                     if msg_content == 'yes':
-                        # Remove the user entry from the Google Sheet for yes responses
                         await self.sheet_fetcher.remove_user_entry(username)
                         await message.author.send("Thank you for your response. Best wishes!")
                     elif msg_content == 'no':
-                        # For no' responses, let the scheduler decide the next contact time
                         await message.author.send("We will check back with you later. Thank you!")
                     else:
-                        # Handle invalid input
                         await message.author.send("Invalid input. Please enter 'Yes' or 'No'.")
 
                 # Regardless of the response, remove the user from awaiting responses
                 self.db_manager.remove_user_awaiting_response(username)
                 
-                # Calculate and store the next send time only if needed
                 if msg_content != 'yes':  
                     self.scheduler.calculate_and_store_next_send_time(username, datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f'))
 
